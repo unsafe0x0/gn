c9eb6ce2289927aa902e146a475056ea92a424f8
unsafe0x0 <priyannxhuu@gmail.com>
2025-10-04 10:36:49 +0530

    dot6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const bodyFont = Inter({
   variable: "--font-geist-sans",
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={bodyFont.className}>{children}</body>
+      <body className={bodyFont.className}>
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
